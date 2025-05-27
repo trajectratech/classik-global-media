f4a852246588cd10533e451dd036d8c28a968c4b
Ikechukwu Peter <60905186+ikechukwu-peter@users.noreply.github.com>
2025-05-27 13:54:47 +0100

    fix: robots.txt

--- src/components/featured-collections/index.tsx
+++ src/components/featured-collections/index.tsx
@@ -8,9 +8,9 @@ export const FeaturedCollections = ({ products }: { products: IProduct[] }) => {
   return (
     <section className="bg-gray-100 py-12" id="featured">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-10">
+        <h1 className="text-3xl font-bold text-center mb-10">
           Featured Collections
-        </h2>
+        </h1>
 
         <ProductSlider featuredProducts={products} />
       </div>
